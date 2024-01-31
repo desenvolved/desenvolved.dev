5d9de84d9904e0d098689193fb626271de62dee9
Ana Paula Assunção <anap.assuncao03@gmail.com>
2024-01-31 18:25:26 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleModel() {
+  let html = document.documentElement
+  html.classList.toggle("light")
+
+  // selecionar tag imagem
+  let img = document.querySelector("#profile img")
+  // se tiver on ligth mode, adicionar a imagem light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver off light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
